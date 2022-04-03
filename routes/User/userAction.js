9e281725a3cfec03fdb774a6982032347fef77ab
VijaiNatesh <glennvijay@gmail.com>
2022-04-03 10:49:46 +0500

    mullet

--- routes/User/userAction.js
+++ routes/User/userAction.js
@@ -155,7 +155,7 @@ userAction.get('/friend_request/:requestId/decline', async (req, res) => {
 // storage
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './uploads')
+      cb(null, '/uploads')
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
