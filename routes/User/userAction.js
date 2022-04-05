a7248cebe625c8ffcfb39583e4383fb7a0053c02
VijaiNatesh <glennvijay@gmail.com>
2022-04-05 06:20:25 +0500

    image

--- routes/User/userAction.js
+++ routes/User/userAction.js
@@ -172,7 +172,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage, fileFilter })
 
 userAction.put('/profile_pic/update', upload.single('profileImage'), async (req, res) => {
-    const userId  = req.file.userId;
+    const userId  = req.body.userId;
     const profile_pic = req.file.filename;
     try {
         const user = await User.findById(userId)
